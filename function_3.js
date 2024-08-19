22d7c7e439f5feb0c7f57202be77c8a9b27877dd
Melnyk Yevhenii <karimovzena457@gmail.com>
2024-08-19 11:08:29 +0300

    homework completed

--- function_3.js
+++ function_3.js
@@ -1,2 +1,9 @@
-function findGCD(a, b,){
+function findGCD(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
+console.log(findGCD(19, 18))
